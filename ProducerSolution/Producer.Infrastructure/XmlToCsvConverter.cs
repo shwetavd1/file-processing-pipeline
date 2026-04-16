0f5179493bea322e9ab9aba8804be73a7d993a2a
dalvi <shweta.dalvi1@zensar.com>
2026-04-16 21:18:39 +0530

    Added comments

--- ProducerSolution/Producer.Infrastructure/XmlToCsvConverter.cs
+++ ProducerSolution/Producer.Infrastructure/XmlToCsvConverter.cs
@@ -7,13 +7,21 @@ namespace Producer.Infrastructure
 {
     public class XmlToCsvConverter : IConverterStrategy
     {
-        // used aysnc because file processing takes time, and we do not want to wait for it
+        // used aysnc because file processing takes time
+        // method returns csv content as string
         public async Task<string> ConvertAsync(string xmlContent)
         {
-            //converts string 
+            /* converts to string
+             * XDocument - built in class represents entire XML used for XML parsing
+             * it stores - root element, child element, element names, values
+             * parses the string into objects - XElement
+            */
             var doc = XDocument.Parse(xmlContent);
 
             //loops records
+            /* descendants - traverses xml tree and returns all child elements 
+             * it returns collection XElement
+            */
             var records = doc.Descendants("Person").Select(x => new
             {
                 Id = x.Element("Id")?.Value,
